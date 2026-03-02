76ca2578d055e81835c6bc0d34ba1f9dd56edf63
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-03-02 10:49:55 -0300

    Add files via upload

---
+++ idade.js
@@ -0,0 +1,7 @@
+let idade = 20;
+let dataNascimento = 2008;
+
+//console.log("Minha idade é: " + idade);
+//console.log("Meu ano de nascimento é: " + dataNascimento);
+console.log(`Minha idade é: ${idade} e meu ano de nascimento é: ${dataNascimento}`);
+
