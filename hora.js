29f8c12a31330a2c0d74ccd7a2c1fdbdf29b067b
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-03-11 09:42:42 -0300

    Add files via upload

--- hora.js
+++ hora.js
@@ -1,3 +1,12 @@
-//Valores Iniciais do dia
+// Valores iniciais do dia
+let valorPorHora = 50;
+let horasTrabalhadas = 8; // Expediente padrão
 
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
 
+horasTrabalhadas++;
+console.log(`Após hora extra (horasTrabalhadas++): ${horasTrabalhadas}h`);
+
+horasTrabalhadas--;
+console.log(`Após saída antecipada: ${horasTrabalhadas}h`);
