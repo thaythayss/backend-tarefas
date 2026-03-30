468e1b30878cefd0d5313ec12b51b33636dcc26e
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-03-30 13:50:05 +0000

    versao2

--- cnh.js
+++ cnh.js
@@ -1,3 +1,12 @@
-const idade =15;
+const idade = 19;
 const maiorIdade = idade >= 18;
 const possuiCNH = false;
+
+const podeDirigir = maiorIdade && possuiCNH; //AND
+console.log('Pode viajar sozinho?', podeDirigir);
+
+const podeViajarSozinho =  maiorIdade || possuiCNH; //OU, OR
+console.log('Pode viajar sozinho?', podeViajarSozinho);
+
+const precisaDeAcompanhante = !maiorIdade;
+console.log('Precisa e acompanhante?', precisaDeAcompanhante);
