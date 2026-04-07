783b685aaf3d976ff7d1df840ccd82863642a37c
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-04-07 11:51:03 +0000

    versao4

---
+++ pontuacao.js
@@ -0,0 +1,11 @@
+//Sistema de pontuação um jogo
+const nomeJogador = "Elisa";
+let faseAtual = 3;
+let vidasRestantes = 2;
+
+let pontosPorMissao = [85, 90, 60, 100];
+
+//pontosPorMissao.length
+
+console.log(`Seja bem vida ${nomeJogador}!`);
+console.log(pontosPorMissao);
