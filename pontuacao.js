7d4ce20d8a0873fd408393684b03a2dbe97cec23
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-04-08 12:30:54 +0000

    versao2

--- pontuacao.js
+++ pontuacao.js
@@ -6,6 +6,16 @@ let vidasRestantes = 2;
 let pontosPorMissao = [85, 90, 60, 100];
 
 //pontosPorMissao.length
+let pontuacaoTotal = 0;
+
+for (let i = 0; i < pontuacoes.length; i++) {
+    pontuacaoTotal = pontuacaoTotal + pontuacoes[i]
+}
+
+//Calculando a média
+let media = pontuacaoTotal / pontuacoes.length; 
+
+let passouDeFase = (media >= 75 && vidas > 0) || (fase === 1);
 
 console.log(`Seja bem vida ${nomeJogador}!`);
 console.log(pontosPorMissao);
