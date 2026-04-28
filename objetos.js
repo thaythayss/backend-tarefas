ba19d04f1289c9960af88df601e0a8906c413bd3
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-04-28 11:51:45 +0000

    versao1

---
+++ objetos.js
@@ -0,0 +1,11 @@
+const bancoAutomotivo = {
+    material: "Couro natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluido: false
+}
+
+console.log("Material escolhido: ",  bancoAutomotivo.material);
+
+bancoAutomotivo.concluido = true;
+console.log(bancoAutomotivo);
